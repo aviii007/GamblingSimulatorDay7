86b92b03b2e6b6233ab96731f79f6dbc62e9902d
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-15 00:29:57 +0530

    Adding the Win or Loose condition.

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -11,5 +11,31 @@ public class Gambler {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulator");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Starting amount of stake : "+AMOUNT_OF_STAKE);
+        System.out.println("Amount to bet after every play : "+BET_AMOUNT);
+        int play = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println("Gamblers play:"+play);
+        int InHandcash = AMOUNT_OF_STAKE;
+        switch(play) {
+            case 1:
+                InHandcash = InHandcash + BET_AMOUNT;
+                System.out.println("Total current cash gambler has "+InHandcash);
+                break;
+            case 2:
+                InHandcash = InHandcash - BET_AMOUNT;
+                System.out.println("Total current cash gambler has "+InHandcash);
+                break;
+            default:
+                System.out.println("default");
+        }
+        if(play == 1) {
+            System.out.println("Player had won the first play");
+        }
+        else {
+            System.out.println("Player had lost the first play");
+        }
+
     }
+
 }
