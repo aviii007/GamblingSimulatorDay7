2e514a8ce164266160459c25b2d7a5af1ec88f0e
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-15 00:17:45 +0530

    Adding the Welcome Message.

---
+++ src/com/bridgelabz/Gambler.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Gambler {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gambling Simulator");
+    }
+}
