0178eefbc7b671a3b4ab3ea374b2370c527625ca
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-15 00:25:42 +0530

    Adding the initial Start stake and Bet Stake.

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -3,6 +3,12 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class Gambler {
+
+    static int AMOUNT_OF_STAKE=100;
+    static int BET_AMOUNT=1;
+    static int WON=1;
+    static int LOST=0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulator");
     }
